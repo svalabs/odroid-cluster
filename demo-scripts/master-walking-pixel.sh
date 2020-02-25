18ad24af0dfecb72c2e23143595d5824fafdb315
root <enrico.bartz@mrel.de>
2020-02-25 10:43:23 +0100

    add some demo scripts

---
+++ demo-scripts/master-walking-pixel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while [ true ]
+do
+  for stack in {1..4}
+  do
+    for node in {1..4}
+    do
+      for led in {0..5}
+      do
+        curl --silent http://mc1-${stack}-${node}:31000/blink?value=${led}.0.0.255
+        curl --silent http://mc1-${stack}-${node}:31000/blink?value=${led}.0.0.0
+      done
+    done
+  done
+done
