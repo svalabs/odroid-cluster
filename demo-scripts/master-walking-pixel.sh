0fd68cfa3b419eacb7cd833373a1f51c4ba7fa24
root <enrico.bartz@mrel.de>
2020-02-26 10:02:32 +0100

    update port

--- demo-scripts/master-walking-pixel.sh
+++ demo-scripts/master-walking-pixel.sh
@@ -8,8 +8,8 @@ do
     do
       for led in {0..5}
       do
-        curl --silent http://mc1-${stack}-${node}:31000/blink?value=${led}.0.0.255
-        curl --silent http://mc1-${stack}-${node}:31000/blink?value=${led}.0.0.0
+        curl --silent http://mc1-${stack}-${node}:5000/blink?value=${led}.0.0.255 > /dev/null
+        curl --silent http://mc1-${stack}-${node}:5000/blink?value=${led}.0.0.0   > /dev/null
       done
     done
   done
